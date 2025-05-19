50b5913de838e0490b20a1795b693e7b39767691
coffin299 <coffin299@ymail.ne.jp>
2025-05-19 19:30:31 +0900

    Fix: [start_arona.bat] fix startarona.bat

--- start_arona.bat
+++ start_arona.bat
@@ -1,11 +1,6 @@
 python -m pip install -U -r requirements.txt
-echo ====================
 echo  ARONA Bot Launcher
-echo ====================
 @echo off
 title ARONA Music BOT Console
-echo.
 python bot.py
-echo.
-echo BOTが停止しました。何かキーを押すとウィンドウを閉じます。
 pause
